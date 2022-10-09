9e01207f469207e2f2f45807c8dcff9898a084f0
anastasia <anyasirman@outlook.com>
2022-10-09 19:14:30 -0400

    added migratons

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-const {sequelize} = require('./models')
+const express = require('express')
+const app = express()
+const {sequelize, User} = require('./models')
+app.use(express.json())
 
-async function main(){
-    await sequelize.sync({force: true})
-}
+app.post('/users', async(req, res)=> {
+    const { name, email, role} = req.body
 
-main()
+    try{
+       const user =  await User.create({name, email, role})
+       return res.json(user)
+    } catch(err){
+        console.log(err)
+        return res.status(500).json(err)
+    }
+
+})
+
+app.get('/users', async(req, res) => {
+    try {
+        const users = User.all
+        return res.json(users)
+    } catch(err){
+        console.log(err)
+    }
+})
+
+
+app.listen({ port: 4000 }, async () => {
+    console.log('server up on')
+    await sequelize.authenticate()
+    console.log('Database connected')
+})
+
