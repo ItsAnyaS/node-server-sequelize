47af07134896a2ed1679300e1b91d7d822121020
anastasia <anyasirman@outlook.com>
2022-10-09 19:43:04 -0400

    added post routes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const {sequelize, User} = require('./models')
+const {sequelize, User, Post} = require('./models')
 app.use(express.json())
 
 app.post('/users', async(req, res)=> {
@@ -23,6 +23,39 @@ app.get('/users', async(req, res) => {
     } catch(err){
         console.log(err)
         return res.status(500).json({error: 'Something went wrong'})
+    }
+})
+
+app.post('/posts', async(req, res) => {
+    const {body, userId} = req.body
+    try {
+        const post = Post.create({body, userId})
+        return res.json(post)
+    } catch(err) {
+        console.log(err)
+        return res.status(500).json(err)
+    }
+})
+
+app.get('/posts/:uuid', async(req, res) => {
+    const uuid = req.params.uuid
+    try{
+        const post = await Post.findOne({
+            where: {uuid}
+        })
+        return res.json(post)
+    }catch(err){
+        console.log(err)
+        return res.status(500).json(err)
+    }
+})
+
+app.get('/posts', async(req, res) => {
+    try {
+        const posts = await Post.findAll()
+        return res.json(posts)
+    } catch(err){
+        console.log(err)
     }
 })
 
